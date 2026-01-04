9aa02d6e5b0d20f754a4bca06dabee05a5dc6bff
Hoo-Sulyn <bsse2509244@peninsulamalaysia.edu.my>
2026-01-04 17:11:19 +0800

    Customer Menu Page (Frontend)

---
+++ ShipEats/app/src/main/java/com/example/shipeatscustomer/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.shipeatscustomer;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MaterialButton admin_option;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        admin_option = findViewById(R.id.admin_option);
+
+        admin_option.setOnClickListener(v -> startActivity(new Intent(this, A1_Login_Page.class)));
+    }
+}
