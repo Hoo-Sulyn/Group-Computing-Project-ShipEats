f10d709a2f04cb958572ef2ecd77ae328f7b9b69
Hoo-Sulyn <bsse2509244@peninsulamalaysia.edu.my>
2026-01-06 21:32:25 +0800

    Done Customer Main Page (Frontend)

--- ShipEats/app/src/main/java/com/example/shipeatscustomer/C3_Menu_Page.java
+++ ShipEats/app/src/main/java/com/example/shipeatscustomer/C3_Menu_Page.java
@@ -1,6 +1,10 @@
 package com.example.shipeatscustomer;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,18 @@ public class C3_Menu_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_c3_menu_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageView menu_icon = findViewById(R.id.menu_icon);
+        TextView menu_text = findViewById(R.id.menu_text);
+        ImageView history_icon = findViewById(R.id.history_icon);
+        TextView history_text = findViewById(R.id.history_text);
+        ImageView settings_icon = findViewById(R.id.settings_icon);
+        TextView settings_text = findViewById(R.id.settings_text);
+
+        //CHANGE FOOTER BUTTON COLOR
+        int activeColor = Color.parseColor("#FFD700");
+        menu_icon.setColorFilter(activeColor);
+        menu_text.setTextColor(activeColor);
+        menu_text.setTypeface(null, Typeface.BOLD);
     }
 }
